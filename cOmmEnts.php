be810f09545d485419760c057599f4d0638a327e
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-07-02 12:13:40 +0300

    COMMENTS

---
+++ cOmmEnts.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+	<h1>COMMENTS</h1>
+	<body>
+	<?php
+	/*Multiline
+	 Comment*/
+	 echo "Single line comment 1 (//)<br>";//exampl 1
+	 echo "Single line comment 2 (#)<br>";#exampl 2
+	 ?>
+	</body>
+</html>
